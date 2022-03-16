92edd03d146274a6ce22acaa1014c2570ebc94a6
Sydney Prier <priersr@gmail.com>
2022-03-16 11:12:31 -0500

    Add files via upload

--- Robot.java
+++ Robot.java
@@ -5,71 +5,116 @@
 package frc.robot;
 
 
-import edu.wpi.first.wpilibj.DoubleSolenoid;
-import edu.wpi.first.wpilibj.PneumaticsModuleType;
-//import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj.XboxController;
 
-
-
-
-
-
-
-
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
 /**
- * This is a sample program showing the use of the solenoid classes during operator control. Three
- * buttons from a joystick will be used to control two solenoids: One button to control the position
- * of a single solenoid and the other two buttons to control a double solenoid. Single solenoids can
- * either be on or off, such that the air diverted through them goes through either one channel or
- * the other. Double solenoids have three states: Off, Forward, and Reverse. Forward and Reverse
- * divert the air through the two channels and correspond to the on and off of a single solenoid,
- * but a double solenoid can also be "off", where the solenoid will remain in its default power off
- * state. Additionally, double solenoids take up two channels on your PCM whereas single solenoids
- * only take a single channel.
+ * The VM is configured to automatically run this class, and to call the functions corresponding to
+ * each mode, as described in the TimedRobot documentation. If you change the name of this class or
+ * the package after creating this project, you must also update the build.gradle file in the
+ * project.
  */
 public class Robot extends TimedRobot {
 
-  public final DoubleSolenoid m_doubleSolenoid =
-  new DoubleSolenoid(PneumaticsModuleType.CTREPCM, 4, 5 );
-
-//private static final int kSolenoidButton = 1;
-public static final int kDoubleSolenoidForward = 2;
-public static final int kDoubleSolenoidReverse = 3;
- public static XboxController driverjoystick;
   // Solenoid corresponds to a single solenoid.
- // private final Solenoid m_solenoid = new Solenoid(PneumaticsModuleType.CTREPCM, 0);
-
+  
+  
   // DoubleSolenoid corresponds to a double solenoid.
-
  
 
   
+  private Command m_autonomousCommand;
+
+  private RobotContainer m_robotContainer;
+
+  /**
+   * This function is run when the robot is first started up and should be used for any
+   * initialization code.
+   */
+  @Override
+  public void robotInit() {
+    // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
+    // autonomous chooser on the dashboard.
+    m_robotContainer = new RobotContainer();
+  }
+
+  /**
+   * This function is called every robot packet, no matter the mode. Use this for items like
+   * diagnostics that you want ran during disabled, autonomous, teleoperated and test.
+   *
+   * <p>This runs after the mode specific periodic functions, but before LiveWindow and
+   * SmartDashboard integrated updating.
+   */
+  @Override
+  public void robotPeriodic() {
+    // Runs the Scheduler.  This is responsible for polling buttons, adding newly-scheduled
+    // commands, running already-scheduled commands, removing finished or interrupted commands,
+    // and running subsystem periodic() methods.  This must be called from the robot's periodic
+    // block in order for anything in the Command-based framework to work.
+    CommandScheduler.getInstance().run();
+  }
+
+  /** This function is called once each time the robot enters Disabled mode. */
+  @Override
+  public void disabledInit() {}
 
   @Override
-  public void teleopPeriodic() {
+  public void disabledPeriodic() {}
 
-    if (driverjoystick.getRawButton(kDoubleSolenoidForward)) {
-      m_doubleSolenoid.set(DoubleSolenoid.Value.kForward);
-    } else if (driverjoystick.getRawButton(kDoubleSolenoidReverse)) {
-      m_doubleSolenoid.set(DoubleSolenoid.Value.kReverse);
+  /** This autonomous runs the autonomous command selected by your {@link RobotContainer} class. */
+  @Override
+  public void autonomousInit() {
+    m_autonomousCommand = m_robotContainer.getAutonomousCommand();
+
+    // schedule the autonomous command (example)
+    if (m_autonomousCommand != null) {
+      m_autonomousCommand.schedule();
     }
+  }
+
+  /** This function is called periodically during autonomous. */
+  @Override
+  public void autonomousPeriodic() {}
+
+  @Override
+  public void teleopInit() {
+    // This makes sure that the autonomous stops running when
+    // teleop starts running. If you want the autonomous to
+    // continue until interrupted by another command, remove
+    // this line or comment it out.
+    if (m_autonomousCommand != null) {
+      m_autonomousCommand.cancel();
+    }
+  }
+
+  /** This function is called periodically during operator control. */
+  @Override
+  public void teleopPeriodic() {
+   
     /*
      * The output of GetRawButton is true/false depending on whether
      * the button is pressed; Set takes a boolean for whether
      * to use the default (false) channel or the other (true).
      */
-   // m_solenoid.set(m_stick.getRawButton(kSolenoidButton));
+  
 
     /*
      * In order to set the double solenoid, if just one button
      * is pressed, set the solenoid to correspond to that button.
      * If both are pressed, set the solenoid will be set to Forwards.
-     *
      */
-
-  
+    
   }
+
+  @Override
+  public void testInit() {
+    // Cancels all running commands at the start of test mode.
+    CommandScheduler.getInstance().cancelAll();
+  }
+
+  /** This function is called periodically during test mode. */
+  @Override
+  public void testPeriodic() {}
 }
--- RobotContainer.java
+++ RobotContainer.java
@@ -6,15 +6,18 @@ package frc.robot;
 
 
 import edu.wpi.first.wpilibj.Compressor;
-
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.commands.DriveforwardTimed;
 import frc.robot.commands.Drivewithjoysticks;
+import frc.robot.subsystems.Solenoid;
 import frc.robot.subsystems.DriveTrain;
-import frc.robot.subsystems.InnerDrive;
-import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.ShooterMotors;
+import frc.robot.subsystems.intakeDoor;
+
+
 
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
@@ -23,8 +26,8 @@ import edu.wpi.first.wpilibj2.command.Command;
  * subsystems, commands, and button mappings) should be declared here.
  */
 public class RobotContainer {
+
   Compressor Comp = new Compressor( PneumaticsModuleType.CTREPCM);
-  
  
 
 
@@ -35,12 +38,18 @@ public class RobotContainer {
   
 
   private final DriveTrain driveTrain;
-  private final InnerDrive innerDrive;
+  private final ShooterMotors innerDrive;
+  private final intakeDoor intakedoor; 
+  private final Solenoid solenoid;
+  
+ 
+ 
+
  
   private Drivewithjoysticks drivewithjoysticks;
   private final DriveforwardTimed driveforwardTimed;
   public static XboxController driverjoystick;
-  public static Compressor comp;
+  
 
  
 
@@ -57,21 +66,36 @@ public class RobotContainer {
 
 
     driveTrain = new DriveTrain();
-    innerDrive = new InnerDrive();
+    innerDrive = new ShooterMotors();
+    intakedoor = new intakeDoor();
+    solenoid = new Solenoid();
+    
+    
+
+    
    
    
-    drivewithjoysticks = new Drivewithjoysticks(driveTrain, innerDrive);
-    driveforwardTimed = new DriveforwardTimed(driveTrain, innerDrive);
+    drivewithjoysticks = new Drivewithjoysticks(driveTrain, innerDrive, intakedoor, solenoid);
+
+    driveforwardTimed = new DriveforwardTimed(driveTrain, innerDrive, intakedoor);
     driveforwardTimed.addRequirements(driveTrain);
-    driveforwardTimed.addRequirements(innerDrive);
+   
 
     driverjoystick = new XboxController(0);
-    
-    drivewithjoysticks.addRequirements(innerDrive);
-    
-    innerDrive.setDefaultCommand(drivewithjoysticks);
 
    
+    drivewithjoysticks.addRequirements(innerDrive);
+    drivewithjoysticks.addRequirements(intakedoor);
+   
+    
+    
+    
+    innerDrive.setDefaultCommand(drivewithjoysticks);
+    intakedoor.setDefaultCommand(drivewithjoysticks);
+
+
+ 
+  
 
 
     // Configure the button bindings
@@ -79,6 +103,7 @@ public class RobotContainer {
   
   
   }
+ 
   /**
    * Use this method to define your button->command mappings. Buttons can be created by
    * instantiating a {@link GenericHID} or one of its subclasses ({@link
@@ -89,7 +114,6 @@ public class RobotContainer {
    
     
   }
-
   /**
    * Use this to pass the autonomous command to the main {@link Robot} class.
    *
